2fffba0ec188b4be9a2c018d743067d00b329d99
Guyesa <guyesa530@gmail.com>
2023-10-24 19:40:31 +0300

    Add files via upload

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Nav from "./Nav.jsx";
 import Home from "./Home.jsx";
 import Footer from "./Footer.jsx";
+import "./App.css";
 function App() {
   return (
     <>
